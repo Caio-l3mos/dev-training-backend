75dd25567444e71fbe41879dc0cc3e518c2e0db8
Caio-l3mos <Caiogomesl3mos@gmail.com>
2025-11-11 07:24:00 -0300

    chore: initial project setup with Express and environment config

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import app from './app.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+});
+
